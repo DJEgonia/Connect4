fabb31e462c6db74015c4017a6a3ca32d4a226a9
Coder101 <minecraftunderpants2002@gmail.com>
2020-03-01 11:41:40 -0800

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {	// write your code here
+
+        int num_of_rows = 6;
+        int num_of_cols = 7;
+
+        int[][] grid = new int[num_of_rows][num_of_cols];
+
+      for(int j=0;j<6;j++)
+      {
+          for(int i = 0; i < 7; i++) {
+              grid[j][i] = 0;
+              System.out.print(grid[j][i]);
+          }
+          System.out.println();
+      }
+
+    }
+
+}
